79af2c8125de5459bd8541647322445ee9742d2d
Lida Deineko <93deineko@gmail.com>
2022-08-03 16:03:13 +0300

    Add changes befer revert

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ console.log('It`s the master branch');
 console.log('Add changes in the master branch');
 
 console.log('Add changes in the master branch1');
+
+console.log('Add changes before revert');
